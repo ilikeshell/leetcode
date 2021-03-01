7457e328b38a1a8eed467c74e5e654c1f930074c
ilikeshell <1007965673@qq.com>
2021-03-01 10:26:42 +0800

    数组，前缀和，去掉nums[].

--- src/Solution303.java
+++ src/Solution303.java
@@ -29,4 +29,28 @@ class NumArray
     }
 }
 
+class NumArray1
+{
+    private int[] sums;
+
+    public NumArray1(int[] nums)
+    {
+        this.sums = new int[nums.length+1];
+
+        if(nums.length > 0)
+        {
+            sums[0] = 0;
+            sums[1] = nums[0];
+            for (int i = 1; i < nums.length; i++)
+            {
+                sums[i+1] = nums[i] + sums[i];
+            }
+        }
+    }
+
+    public int sumRange(int i, int j)
+    {
+        return sums[j+1] - sums[i];
+    }
+}
 
