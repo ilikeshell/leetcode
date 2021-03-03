033c6799b32a769155c7ddb461a237e66736342d
ilikeshell <1007965673@qq.com>
2021-03-03 11:17:43 +0800

    比特位计数，新增了一种O(n)时间复杂度的方法。

---
+++ src/Solution338.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Solution338
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Arrays.toString(new Solution338().countBits(2)));
+    }
+
+    public int[] countBits(int num)
+    {
+        int[] ret = new int[num + 1];
+
+        for (int i = 0; i <= num; i++)
+        {
+            ret[i] = countOnes(i);
+        }
+
+        return ret;
+    }
+
+    public int countOnes(int x)
+    {
+        int ones = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            if ((x >> i & 1) == 1)
+                ones++;
+        }
+        return ones;
+    }
+
+    public int[] countBits2(int num)
+    {
+        int[] ret = new int[num + 1];
+        ret[0] = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            ret[i] = (i & 1) == 1 ? ret[i - 1] + 1 : ret[i / 2];
+        }
+
+        return ret;
+    }
+}
