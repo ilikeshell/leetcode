9ef09e5761fa79ef06fb04a9f7261f33d01c6a0e
ilikeshell <1007965673@qq.com>
2021-03-20 23:58:38 +0800

    第55题，跳跃游戏，采用了卫东老师的方法。

--- src/Solution55.java
+++ src/Solution55.java
@@ -1,15 +1,46 @@
+import java.util.Arrays;
+
 public class Solution55
 {
-    public boolean canJump(int[] nums)
+    public boolean canJump1(int[] nums)
     {
+        if(nums == null || nums.length == 0)
+            return false;
         Boolean[] dp = new Boolean[nums.length];
+        Arrays.fill(dp, false);
         dp[0] = true;
 
         for (int i = 0; i < dp.length; i++)
         {
-
+            for (int j = 1; j <= nums[i] && i + j < nums.length; j++)
+            {
+                dp[i + j] = true;
+            }
         }
 
+        return dp[nums.length - 1];
+    }
+
+    public boolean canJump(int[] nums)
+    {
+        if(nums == null || nums.length == 0)
+            return false;
+
+        Boolean[] dp = new Boolean[nums.length];
+        dp[0] = true;
+
+        for (int i = 1; i < dp.length; i++)
+        {
+            dp[i] = false;
+            for(int j = 0; j < i; j++)
+            {
+                if(dp[j] && j + nums[j] >= i)
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
         return dp[nums.length - 1];
     }
 }
