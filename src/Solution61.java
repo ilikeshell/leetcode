93276d39dd1545d002d5160b2443ee9974cf0e79
ilikeshell <1007965673@qq.com>
2021-03-27 08:11:44 +0800

    第61题旋转链表。

--- src/Solution61.java
+++ src/Solution61.java
@@ -8,6 +8,7 @@ public class Solution61
         int len = 1;
         ListNode moveP = head, tailP;
 
+        /** 计算链表长度并保存尾节点 */
         while (moveP.next != null)
         {
             moveP = moveP.next;
@@ -17,18 +18,22 @@ public class Solution61
         tailP = moveP;
         moveP = head;
 
+        /** 找到倒数第K%len个元素，需找到前 len - (k % len) 个元素 */
         k = len - (k % len);
 
+        /** 查找 */
         while(k > 1)
         {
             moveP = moveP.next;
             k--;
         }
 
+        /** 修改指针指向 */
         tailP.next = head;
         head = moveP.next;
         moveP.next = null;
 
+        /** 返回新的链表头 */
         return head;
     }
 }
