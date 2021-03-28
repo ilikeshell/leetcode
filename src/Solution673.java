7a8fc90139f29df46f7c4765a3124673ca9ea21d
ilikeshell <1007965673@qq.com>
2021-03-28 21:16:55 +0800

    完成第673题最长递增子序列的个数,这个题需要两个维度，一个维度记录子序列的长度，一个维度记录当前长度子序列的个数。需要考虑的细节较多，还需仔细揣摩。另外也可尝试利用回溯法去判断，不知是否成功？

--- src/Solution673.java
+++ src/Solution673.java
@@ -6,7 +6,8 @@ public class Solution673
 
     public static void main(String[] args)
     {
-        int[] nums = {1, 2, 4, 3, 5, 4, 7, 2};
+        //int[] nums = {1, 2, 4, 3, 5, 4, 7, 2};
+        int[] nums = {2,2,2,2,2};
         System.out.println(findNumberOfLIS(nums));
     }
 
@@ -15,31 +16,45 @@ public class Solution673
         if (nums == null || nums.length == 0)
             return 0;
 
-        int[] f = new int[nums.length];
-        f[0] = 1;
+        int[][] f = new int[2][nums.length];
+        f[0][0] = f[1][0] = 1;
 
         /** 如果设置为0， 而数组长度为1，for循环可能不会执行 */
-        int ret = 1; //不为空的话，长度至少为1
+        int maxLen = 1; //不为空的话，长度至少为1
 
         for (int i = 1; i < nums.length; i++)
         {
-            f[i] = 1;
+            f[0][i] = 1;
+            f[1][i] = 1;
+            int preMax = Integer.MIN_VALUE;
 
             for (int j = i - 1; j >= 0; j--)
             {
-                if (nums[i] > nums[j])
+                if (nums[i] > nums[j] && f[0][j] >= preMax)
                 {
-                    f[i] = f[j] + 1;
-                    break;
+                    if(f[0][j] > preMax)
+                    {
+                        preMax = f[0][j];
+                        f[0][i] = preMax + 1;
+                        f[1][i] = f[1][j];
+                    }
+                    else {
+                        f[1][i] += f[1][j];
+                    }
                 }
             }
-
-            ret = Math.max(ret, f[i]);
+            maxLen = Math.max(maxLen, f[0][i]);
         }
-        //System.out.println(Arrays.toString(f));
-        //return ret;
+        //System.out.println(Arrays.toString(f[0]));
+        //System.out.println(Arrays.toString(f[1]));
 
-
+        int count = 0;
+        for (int i = 0; i < f[0].length; i++)
+        {
+            if(f[0][i] == maxLen)
+                count += f[1][i];
+        }
+        return count;
     }
 
     private static void bt(int[] nums, int[] f, int prevIndex, int n, int max)
@@ -67,4 +82,5 @@ public class Solution673
         }
 
     }
+
 }
