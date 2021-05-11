4f5e36a2ca5598d5d8ec96a69471d619d88a638c
ilikeshell <1007965673@qq.com>
2021-05-11 10:30:44 +0800

    解决第872题叶子相似的树

--- src/Solution872.java
+++ src/Solution872.java
@@ -63,21 +63,23 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class Solution872 {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         List<Integer> l1 = new ArrayList<>(), l2 = new ArrayList<>();
